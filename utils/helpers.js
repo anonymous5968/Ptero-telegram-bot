c48b9720f3ee6b43a13a7f0e6b1bb5fd44c2c35a
anonymous5968 <159430722+anonymous5968@users.noreply.github.com>
2026-01-06 13:17:36 +0300

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,10 @@
 
+const db = require('../firebase');
+
+async function isPaid(telegramId) {
+  const userRef = db.collection('users').doc(telegramId.toString());
+  const doc = await userRef.get();
+  return doc.exists && doc.data().paid === true;
+}
+
+module.exports = { isPaid };
