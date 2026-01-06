93ef18095e037a3db829ec13daebb55a1750c11a
anonymous5968 <159430722+anonymous5968@users.noreply.github.com>
2026-01-06 13:20:19 +0300

    Update admincommands.js

--- commands/admincommands.js
+++ commands/admincommands.js
@@ -1,1 +1,12 @@
 
+const db = require('../firebase');
+
+async function approveUser(bot, msg, args) {
+  const telegramId = args[0];
+  if (!telegramId) return bot.sendMessage(msg.chat.id, "Usage: /approve <telegramId>");
+
+  await db.collection('users').doc(telegramId.toString()).set({ paid: true }, { merge: true });
+  bot.sendMessage(msg.chat.id, `User ${telegramId} marked as paid.`);
+}
+
+module.exports = { approveUser };
